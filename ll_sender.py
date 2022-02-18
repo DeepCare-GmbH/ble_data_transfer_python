1ab56f99d7540a39f1f1e4aa04441f53a246dd93
Michael Katzenberger <forum@mibcat.de>
2022-02-18 22:44:58 +0100

    ISA-292: implement low level file upload

--- ll_sender.py
+++ ll_sender.py
@@ -2,72 +2,82 @@
 import hashlib
 import itertools
 import logging
+from math import ceil
 from typing import List
 
 import coloredlogs
 
 from ble_data_transfer_python.gen.deepcare.transfer_data import TransferData
 
-PAYLOAD_HEADER_SIZE = 22
 
+class LLSender():
 
-class Sender():
+    PAYLOAD_HEADER_SIZE = 22
 
     def __init__(self, mtu=185) -> None:
+
+        # take over MTU
         self._mtu = mtu
+        # transfer data instance
+        self._transfer_data = TransferData()
 
-    def send_string(self, string: str) -> List[TransferData]:
-
-        transfer_data: List[TransferData] = []
-
-        # slice data into chunks of maximum payload
-        split = self._split(string)
-
-        # generate final protobuf list
-        for index, item in enumerate(split):
-            transfer_data.append(
-                TransferData(
-                    current_chunk=index,
-                    overall_chunks=len(split),
-                    hash=hashlib.md5(item).digest()[0:2],
-                    data=item,
-                )
-            )
-
-        return transfer_data
-
-    def _split(self, string: str) -> List[bytes]:
-
-        result: List[bytes] = []
-
-        # generate iterator from data
-        it_string = iter(bytes(string, 'utf-8'))
+    def send(self, data: List[bytes]):
 
         # determine payload size
-        payload_size = self._mtu - PAYLOAD_HEADER_SIZE
+        payload_size = self._mtu - self.PAYLOAD_HEADER_SIZE
 
-        # split data now - create first slice
-        item = bytes(itertools.islice(it_string, payload_size))
-        while len(item):
-            assert (len(item) <= payload_size), 'payload size error'
-            # add slice
-            result.append(item)
-            # generate next slice
-            item = bytes(itertools.islice(it_string, payload_size))
+        # reset transfer data
+        self._transfer_data.current_chunk = -1
+        self._transfer_data.overall_chunks = ceil(
+            len(data) / payload_size)
 
-        return result
+        # create generator
+        self._payload = self._split(data, payload_size)
+
+    def get_chunk(self) -> TransferData:
+
+        try:
+            # return next chunk
+            return self._payload.__next__()
+
+        except StopIteration:
+            # no more chunks available
+            return TransferData()
+
+    def _split(self, data: List[bytes], size: int) -> bytes:
+
+        # generate iterator from data
+        it_data = iter(data)
+
+        while True:
+            # get next chunk of data
+            chunk = bytes(itertools.islice(it_data, size))
+
+            if not chunk:
+                # no more data available
+                break
+
+            # fill transfer data
+            self._transfer_data.current_chunk += 1
+            self._transfer_data.hash = hashlib.md5(chunk).digest()[0:2]
+            self._transfer_data.data = chunk
+
+            # return transfer data
+            yield self._transfer_data
 
 
 if __name__ == '__main__':
 
     logging.basicConfig(level=logging.INFO)
-    log = logging.getLogger('transceiver')
+    log = logging.getLogger('ll_sender')
 
     coloredlogs.install(logger=log)
 
     log.info('transceiver test')
 
-    sender = Sender()
+    MTU = 50
+
+    sender = LLSender(mtu=MTU)
 
     # cSpell:disable
     S1000 = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ' \
@@ -82,6 +92,24 @@ if __name__ == '__main__':
         'molestie consequat, vel illum dolore eu feu'
     # cSpell:enable
 
-    log.info('test data contains %d chars', len(S1000))
-    data = sender.send_string(S1000)
-    log.info('this leads to %d transfer chunks', len(data))
+    log.info('test transmitting')
+
+    sender.send(list(bytes(S1000, 'UTF-8')))
+
+    expected_chunks = ceil(len(S1000) / (MTU - PAYLOAD_HEADER_SIZE))
+
+    received = bytes()
+    count = 0
+    while True:
+        test_chunk = sender.get_chunk()
+        if test_chunk == TransferData():
+            break
+        print(test_chunk)
+        received += test_chunk.data
+        count += 1
+        assert expected_chunks == test_chunk.overall_chunks, 'wrong number of chunks'
+
+    assert count == expected_chunks, 'wrong number of chunks'
+    assert bytes(S1000, 'UTF-8') == received, 'split error'
+
+    log.info('test finished')
