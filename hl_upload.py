b87c40c035460a035965db1fcdb065f2266bf3a6
Michael Katzenberger <forum@mibcat.de>
2022-03-06 17:52:26 +0100

    ISA-339: enhance hl_uploader: imporve chunk generator, implement transfer duration, remove warnings,

--- hl_upload.py
+++ hl_upload.py
@@ -8,7 +8,7 @@ import hashlib
 import logging
 import pathlib
 from math import ceil
-from typing import List
+import time
 
 import coloredlogs
 
@@ -37,6 +37,9 @@ class HLUpload:
         # current response
         self._response = StartTransferResponse()
 
+        # time stamp the data transfer was initiated, contains duration after transfer
+        self._timestamp = 0.0
+
         self._chunk_generator: bytes = None
 
     def _reset(self, request: StartTransferRequest):
@@ -60,16 +63,20 @@ class HLUpload:
         # transfer is now active
         self._response.status = StartTransferResponseStatus.TRANSFER
 
+        # take timestamp
+        self._timestamp = time.time()
+
         # create generator
         self._chunk_generator = self._split(upload_file)
 
     def _split(self, file_name: pathlib.Path) -> bytes:
 
         with open(file_name, 'rb') as f_in:
-            data = bytes(1)
-            while len(data) > 0:
+            while True:
                 data = f_in.read(self._chunk_size)
                 yield data
+                if len(data) != self._chunk_size:
+                    break
 
     def set_request(self, request: StartTransferRequest) -> None:
 
@@ -80,8 +87,14 @@ class HLUpload:
             self._reset(request)
 
         else:
+            # check if more junks are read than available
+            if self._response.next_chunk >= self._response.chunks:
+                self._logger.error('no more chunks available')
+
             # next chunk
             self._response.next_chunk += 1
+
+        self._logger.debug(self._request)
 
     def get_response(self) -> StartTransferResponse:
 
@@ -95,8 +108,39 @@ class HLUpload:
             except StopIteration:
                 # no more chunks available
                 self._response.status = StartTransferResponseStatus.FINISHED
+                # reset hash
+                self._response.hash = bytes()
+                # stop time
+                self._timestamp = time.time() - self._timestamp
+
+        # update transfer time
+        self._response.duration = self.transfer_duration
+
+        self._logger.debug(self._response)
 
         return self._response
+
+    @property
+    def transfer_duration(self) -> float:
+        """Duration of last transfer.
+
+        Returns:
+            float: duration of file transfer in [s]
+        """
+
+        # if transfer in progress return time since start
+        if self._response.status == StartTransferResponseStatus.TRANSFER:
+            duration = time.time() - self._timestamp
+
+        # return the duration of the last transfer
+        elif self._response.status == StartTransferResponseStatus.FINISHED:
+            duration = self._timestamp
+
+        # in all other cases return zero
+        else:
+            duration = 0.0
+
+        return round(duration, 2)
 
 
 if __name__ == '__main__':
@@ -113,30 +157,36 @@ if __name__ == '__main__':
     test_dst_folder = pathlib.Path('/home/pi/')
 
     sender = LLSender()
-    hl_upload = HLUpload(test_dst_folder, sender)
+    hl_upload = HLUpload(test_dst_folder, sender, 1024*2)
 
     # copy test file to upload folder
     test_dst_folder.joinpath(
         'upload', test_src_file.parts[-1]).write_bytes(test_src_file.read_bytes())
 
-    request = StartTransferRequest(filename=test_src_file.parts[-1])
+    test_request = StartTransferRequest(filename=test_src_file.parts[-1])
 
-    hl_upload.set_request((request))
-
-    response = hl_upload.get_response()
+    hl_upload.set_request((test_request))
 
     received = bytes()
 
-    while response.status != StartTransferResponseStatus.FINISHED:
-        log.info(response)
+    test_response = hl_upload.get_response()
+
+    for n in range(test_response.chunks):
+
+        log.info('read file chunk %d ...', n)
         while True:
             test_chunk = sender.get_chunk()
             if test_chunk == TransferData():
                 break
-            log.info(test_chunk)
+            log.info('transfer data: chunks %d/%d',
+                     test_chunk.current_chunk+1, test_chunk.overall_chunks)
             received += test_chunk.data
 
-        response = hl_upload.get_response()
+        log.info('request next file chunk')
+        test_request.hash = test_response.hash
+        hl_upload.set_request((test_request))
+
+        test_response = hl_upload.get_response()
 
     original = test_src_file.read_bytes()
     assert original == received, 'test failed'
